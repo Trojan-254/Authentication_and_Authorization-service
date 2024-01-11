66da5c67df7e4054e1dfbfe21e86242912f2c753
Samwuels simiyu <samwuels254@gmail.com>
2024-01-11 09:06:16 +0300

    User registration

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const bodyParser = require('body-parser');
+const authRouter = require('./routes/auth');
+
+
+port = 3000;
+
+/* Middlewares */
+
+app.use('/api/auth', authRouter);
+
+/* Database connection */
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("Database connection succesfull...!"))
+.catch((err) => {
+    console.log(err);
+});
+
+/* Backend server connection */
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is up and running on port ${port}...`)
+})
---
+++ server/routes/auth.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+var jwt = require('jsonwebtoken');
+
+/* USER REGISTRATION */
+
+router.post = ('/register', async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECURE).toString(),
+    });
+
+    try{
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+        const savedUserName = savedUser.email
+        console.log(`${savedUserName} has registered succesfully`);
+    } catch (err) {
+        res.status(500).json({ message: "Error registering user", err})
+    }
+});
+
+
+/* Module export */
+
+module.exports = router;
