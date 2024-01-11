802b2e9162a51af1f9c07221e6abafccb78c559b
Samwuels simiyu <samwuels254@gmail.com>
2024-01-11 10:51:33 +0300

    Succesful user login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -23,6 +23,29 @@ router.post('/register', async (req, res) => {
 });
 
 
+/* USER LOGIN */
+
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(401).json("Email not found!")
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECURE);
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        originalPassword !== req.body.password && res.status(401).json("Wrong password!");
+        const accessToken = jwt.sign({
+            id: user.id
+        }, process.env.JWT_SECURE, { expiresIn: "7d" });
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, accessToken });
+        console.log("Successfull user login!");
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 /* Module export */
 
 module.exports = router;
